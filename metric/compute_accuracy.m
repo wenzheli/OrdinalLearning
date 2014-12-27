9f5b12586cea9f8093bcd144d5f3e2b6c0c15d41
wenzheli <nadalwz1115@hotmail.com>
2014-12-26 20:51:57 -0800

    minor change

--- metric/compute_accuracy.m
+++ metric/compute_accuracy.m
@@ -14,22 +14,17 @@ for i=1:size(va_data,1)
         % construct matrix M to store all the expectations
         M = zeros(D,D);
         for d1=1:D
+            x_d1_s = intervals{d1}(x(d1));
+            x_d1_e = intervals{d1}(x(d1)+1);
+            y_d1_s = intervals{d1}(y(d1));
+            y_d1_e = intervals{d1}(y(d1)+1);
             for d2=1:D                
                 if d1 == d2
-                    x_d1_s = intervals{d1}(x(d1));
-                    x_d1_e = intervals{d1}(x(d1)+1);
-                    y_d1_s = intervals{d1}(y(d1));
-                    y_d1_e = intervals{d1}(y(d1)+1);
                     ext_diff = (x_d1_e-x_d1_s)^2/12 + (x_d1_e+x_d1_s)^2 /4 + (y_d1_e-y_d1_s)^2/12 + (y_d1_e+y_d1_s)^2 /4 - 2 *(x_d1_e+x_d1_s)*(y_d1_e+y_d1_s)/4;
                     
                 else
-                    x_d1_s = intervals{d1}(x(d1));
-                    x_d1_e = intervals{d1}(x(d1)+1);
                     x_d2_s = intervals{d2}(x(d2));
                     x_d2_e = intervals{d2}(x(d2)+1);
-                    
-                    y_d1_s = intervals{d1}(y(d1));
-                    y_d1_e = intervals{d1}(y(d1)+1);
                     y_d2_s = intervals{d2}(y(d2));
                     y_d2_e = intervals{d2}(y(d2)+1);
                     
@@ -52,6 +47,6 @@ for i=1:size(va_data,1)
     end
 end
 
-return correct/total;
+accu = correct/total;
 
 
